0bfb29e7ca7dadff388dfdc8cdbef921e8100fbf
Sriharsha KR <53250729+neo193@users.noreply.github.com>
2023-11-17 17:33:47 +0530

    Fixed login system.

--- login.js
+++ login.js
@@ -3,11 +3,12 @@ require('dotenv').config();
 
 async function Login() {
     const password = process.env.PASSWORD;
+    const email = process.env.EMAIL;
     if (!password) {
         throw new Error('CLASH_OF_CLANS_PASSWORD environment variable not set');
     }
 
-    await client.login({ email: 'sriharshakr3211@gmail.com', password });
+    await client.login({ email, password });
 }
 
 module.exports = { Login };
